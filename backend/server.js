a528bb8b01538a950dbe2374d93cf289aaec9076
Beket <lbeketl@gmail.com>
2020-11-02 20:11:03 +0200

    tinder backend

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+// App config
+const app = express();
+const port = process.env.PORT || 3001; 
+const connection_url = 'mongodb+srv://admin:vas46285va@cluster0.zlbp4.mongodb.net/tinder-clone?retryWrites=true&w=majority'
+import Cors from 'cors';
+
+import Cards from './dbCards.js';
+// Middlewares
+app.use(express.json());
+app.use(Cors());
+
+// DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+// API-Endpoints
+app.get("/", (req, res) => res.status(200).send('Hello clever programmer!'));
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+} );
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
+
+// Listener
+app.listen(port, () => console.log(`listening on localhost ${port}`))
